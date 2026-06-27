c175e9c7ab4072a72526dc4ea1f62dab2813f72a
karimzade_k <karimzade_k@STHQ012D-01>
2026-06-27 12:59:13 +0400

    linq start

--- 1.Linq/ExtentionMethods/CustomExtentions.cs
+++ 1.Linq/ExtentionMethods/CustomExtentions.cs
@@ -2,4 +2,19 @@
 
 public static class CustomExtentions
 {
+    public static void Print(this IEnumerable<int> numbers)
+    {
+        foreach (var item in numbers)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    public static void PrintGeneric<T>(this IEnumerable<T> numbers)
+    {
+        foreach (var item in numbers)
+        {
+            Console.WriteLine(item);
+        }
+    }
 }
--- 1.Linq/Program.cs
+++ 1.Linq/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace _1.Linq
+﻿using _1.Linq.DataAccess;
+using _1.Linq.ExtentionMethods;
+
+namespace _1.Linq;
+
+// LINQ => Generic Extention methodlar toplusu
+
+
+
+
+public class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+
+        IEnumerable<int> numbers = new List<int> { 1, 5, 65, 15, 45, 25, 5, 25 };
+        List<int> numbers2 = new List<int> { 1, 5, 65, 15, 45, 25, 5, 25 };
+
+
+        // Listim unique olsun
+
+        // numbers = numbers.Distinct();
+        // 
+        // foreach (var item in numbers)
+        // {
+        //     Console.WriteLine(item);
+        // }
+
+        // Task: Print Method extention
+
+        // numbers.Distinct().Print();
+
+        // Console.WriteLine(numbers.Min());
+        // Console.WriteLine(numbers.Max());
+        // Console.WriteLine(numbers.Average());
+
+        // numbers2.FindAll(x => x >= 45).Print();
+
+
+        // Class ile islemek
+
+        var students = DB.GetStudents();
+
+        // students.PrintGeneric();
+
+
+        // var students2 = students.AsReadOnly();
+
+        // Score 10-dan yuxari olanlari alim. FindAll
+
+
+
+
     }
 }
